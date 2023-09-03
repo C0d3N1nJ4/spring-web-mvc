754eddd6f335d22649df3d3267ee19185b12fe24
naiomi <Naiomi.Naidoo@icloud.com>
2023-09-03 10:11:44 +0200

    Implementing the domain classes

---
+++ src/main/java/com/example/web/domain/SportType.java
@@ -0,0 +1,34 @@
+package com.example.web.domain;
+
+public class SportType {
+
+    private int id;
+
+    private String name;
+
+    public SportType() {
+    }
+
+    public SportType(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
